f5be9a7c249f5c5ba238fca17afd67283d5fcfd3
cristianccgg <cristianccggg@gmail.com>
2024-08-19 15:02:04 -0500

    Fix JSON import and adjust paths

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import countriesJson from "../countries.json";
 import pattern from "/images/shared/desktop/bg-pattern-small-circle.svg";
 
 export const Countries = () => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch("/countries.json")
+      .then((response) => response.json())
+      .then((data) => setCountries(data))
+      .catch((error) => console.error("Error loading JSON:", error));
+  }, []);
+
   return (
     <div className="flex flex-col gap-[48px] py-[120px] xl:flex-row xl:w-[1110px] xl:justify-between xl:px-14">
-      {countriesJson.map((country, index) => (
+      {countries.map((country, index) => (
         <div className="flex flex-col items-center" key={index}>
           <img
             className="relative mb-[48px]"
